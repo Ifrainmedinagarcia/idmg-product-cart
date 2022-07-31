1b1f322caf6c6018a2c41e8a936b5ca1ab7aaac5
Ifrain <imedinag@achs.cl>
2022-07-30 21:45:49 -0400

    Version 0.0.2

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../dist
 
 export const product = {
   id: '1',
-  title: 'Coffee Mug - Card',
+  title: 'Coffee Mug - Card!',
   // img: './coffee-mug.png'
 }
 
